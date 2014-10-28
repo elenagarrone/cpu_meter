2e614630db99dad48f41f08ef7ff364416bd744c
elena <elena.15@hotmail.it>
2014-10-28 20:00:12 +0000

    os-monitor working and sent data to browser

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require ('socket.io')(http);
 var expressLayouts = require('express-ejs-layouts');
+var spawn = require('child_process').spawn;
+var monitor = require('os-monitor');
+var stream = monitor.start({ delay: 60, stream: true });
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -16,4 +19,15 @@ app.get('/', function(req, res){
 http.listen(2000, function(){
 	console.log('listening on port 2000')
 });
+
+var stream = monitor.start({ delay: 60, stream: true });
+
+
+io.on('connection', function(socket){
+	stream.on('monitor', function(event){
+		var loadavg = Math.floor(event.loadavg[0]*100);
+		socket.emit('usage', {usage: loadavg});
+	});
+});
+
 
